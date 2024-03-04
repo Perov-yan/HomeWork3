428933c7a572c067661ffd5e3e69535114a509e6
Ян <peroff007@gmail.com>
2024-03-04 22:39:45 +0300

    Задача 1 выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Задача 1:");
+        int q = 2147483647;
+        byte w = 127;
+        short e = 32767;
+        long r = 9000000000000001L;
+        float t = 7.123456f;
+        double y = 8.12332112332112;
+        System.out.println("Значение переменной w с типом byte = " + w);
+        System.out.println("Значение переменной e с типом short = " + e);
+        System.out.println("Значение переменной q с типом int = " + q);
+        System.out.println("Значение переменной r с типом long = " + r);
+        System.out.println("Значение переменной t с типом float = " + t);
+        System.out.println("Значение переменной y с типом double = " + y);
+        System.out.println(" ");
 
-        System.out.println("Hello and welcome!");
+
 
     }
 }
