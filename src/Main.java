b4d4e89d0629c04f418a83a1227195bc816582e4
Ян <peroff007@gmail.com>
2024-03-05 01:55:11 +0300

    Задача 8 выполнена

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ public class Main {
         long s = 987678965549L;
         float d = 2.786f;
         short f = 569;
-        short g = - 159;
+        short g = -159;
         short h = 27897;
         byte j = 67;
         System.out.println("Данное значение: " + a + " можно отнести к float");
@@ -94,9 +94,9 @@ public class Main {
         int totalIceCreamWeight = iceCream * iceCreamWeight;
         int totalEggWeight = egg * eggWeight;
         int breakfast = totalBananaWeight + totalMilkWeight + totalIceCreamWeight + totalEggWeight;
-        float totalBreakfast =  breakfast / 1000f;
-        System.out.println("Общий вес завтрака спортсмена в граммах "+ breakfast);
-        System.out.println("Общий вес завтрака спортсмена в килограммах "+ totalBreakfast);
+        float totalBreakfast = breakfast / 1000f;
+        System.out.println("Общий вес завтрака спортсмена в граммах " + breakfast);
+        System.out.println("Общий вес завтрака спортсмена в килограммах " + totalBreakfast);
         System.out.println(" ");
 
         System.out.println("Задача 7:");
@@ -106,10 +106,40 @@ public class Main {
         int totalWeight = weight * 1000;
         int dayLowWeight = totalWeight / lowWeight;
         int dayHighWeight = totalWeight / highWeight;
-        int mediumDay = ( dayHighWeight + dayLowWeight ) / 2;
-        System.out.println( dayLowWeight + " дней уйдет на похудение, если спортсмен будет терять каждый день по " + lowWeight + " грамм или " + dayHighWeight + " дней — если каждый день будет худеть на " + highWeight + " грамм.");
-        System.out.println( mediumDay + " дней в среднем уйдет на похудение");
+        int mediumDay = (dayHighWeight + dayLowWeight) / 2;
+        System.out.println(dayLowWeight + " дней уйдет на похудение, если спортсмен будет терять каждый день по " + lowWeight + " грамм или " + dayHighWeight + " дней — если каждый день будет худеть на " + highWeight + " грамм.");
+        System.out.println(mediumDay + " дней в среднем уйдет на похудение");
         System.out.println(" ");
+
+        System.out.println("Задача 8:");
+        int salaryMasha = 67760;
+        int salaryDenis = 83690;
+        int salaryKristina = 76230;
+        byte year = 12;
+        int salaryIncreaseMasha = salaryMasha * 10 / 100;
+        int increasedSalaryMasha = salaryMasha + salaryIncreaseMasha;
+        int increasedYearSalaryMasha = salaryIncreaseMasha * year;
+        int yearSalaryMasha = salaryMasha * year;
+        int newYearSalaryMasha = increasedSalaryMasha * year;
+
+        int salaryIncreaseDenis = salaryDenis * 10 / 100;
+        int increasedSalaryDenis = salaryDenis + salaryIncreaseDenis;
+        int increasedYearSalaryDenis = salaryIncreaseDenis * year;
+        int yearSalaryDenis = salaryDenis * year;
+        int newYearSalaryDenis = increasedSalaryDenis * year;
+
+        int salaryIncreaseKristina = salaryKristina * 10 / 100;
+        int increasedSalaryKristina = salaryKristina + salaryIncreaseKristina;
+        int increasedYearSalaryKristina = salaryIncreaseKristina * year;
+        int yearSalaryKristina = salaryKristina * year;
+        int newYearSalaryKristina = increasedSalaryKristina * year;
+
+        System.out.println("Маша теперь получает " + increasedSalaryMasha + " рублей. Годовой доход вырос на " + increasedYearSalaryMasha + " рублей");
+        System.out.println("годовая зарплата Маши состовляла " + yearSalaryMasha + " рублей. После получения прибавки годовой доход состовляет " + newYearSalaryMasha + " рублей. Разницу между годовым доходом составила " + increasedYearSalaryMasha + " рублей");
+        System.out.println("Денис теперь получает " + increasedSalaryDenis + " рублей. Годовой доход вырос на " + increasedYearSalaryDenis + " рублей");
+        System.out.println("Годовая зарплата Дениса состовляла " + yearSalaryDenis + " рублей. После получения прибавки годовой доход состовляет " + newYearSalaryDenis + " рублей. Разницу между годовым доходом составила " + increasedYearSalaryDenis + " рублей");
+        System.out.println("Кристина теперь получает " + increasedSalaryKristina + " рублей. Годовой доход вырос на " + increasedYearSalaryKristina + " рублей");
+        System.out.println("Годовая зарплата Кристины состовляла " + yearSalaryKristina + " рублей. После получения прибавки годовой доход состовляет " + newYearSalaryKristina + " рублей. Разницу между годовым доходом составила " + increasedYearSalaryKristina + " рублей");
 
     }
 }
